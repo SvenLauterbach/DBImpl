b483eec1234dc022ccd5c5eed516c28b2c996dbd
Sven <sven@sven-VirtualBox.(none)>
2013-05-12 01:13:49 +0200

    funktioniert: - Laden von Pages/Frames funktioniert.
    ToDo:
    - Löschen von Frames bei vollem Buffer
    - WriteBack
    - locking

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include "buffermanager.h"
+#include "bufferframe.h"
 
 int main(int argc, char **argv) {
-    std::cout << "Hello, world!" << std::endl;
+    
+    BufferManager* manager = new BufferManager("100.db", 2);
+    
+    BufferFrame& frame = manager->fixPage(0, true);
+    
+    int nrElements = PAGE_SIZE / sizeof(uint64_t);
+    
+    uint64_t* data = new uint64_t[nrElements];
+    data = frame.getData();
+    
+    for(int i = 0; i < nrElements; i++)
+    {
+	std::cout << data[i] << std::endl;
+    }
+    
+    BufferFrame& frame2 = manager->fixPage(0, true);
     return 0;
 }
